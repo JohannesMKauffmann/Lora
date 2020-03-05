1f8372bafb039d43fa93b23ba62bd98173f8cb93
Sytze van der Gaag <43172319+SytzeVDGaag@users.noreply.github.com>
2020-03-05 12:50:52 +0100

    Update TempSensorCode.ino
    fix pin en data formatting

--- TempSensorCode.ino
+++ TempSensorCode.ino
@@ -1,18 +1,15 @@
 #include <dht.h>
-#define DHT11_PIN 2
+#define DHT11_PIN 3
 dht DHT;
 
 void setup()
 {         
-  Serial.begin(9600);
+  Serial.begin(115200);
 }
 
 void loop(){
- Serial.print("DHT11, \t");
- int chk = DHT.read11(DHT11_PIN);
- Serial.print((DHT.humidity), 1);
- Serial.print(",\t\t");
- Serial.println((DHT.temperature), 1);
-
+ DHT.read11(DHT11_PIN);
+ String data = String(DHT.humidity, 1) + ',' + String(DHT.temperature, 1);
+ Serial.println(data);
  delay(2000);
 }
