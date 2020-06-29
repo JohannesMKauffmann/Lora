990ad4c43145cd13f26df7b39b3864d3d4f74473
SietzeKoonstra <43609198+SietzeKoonstra@users.noreply.github.com>
2020-06-29 19:45:57 +0200

    werkend project toegevoegd

---
+++ TTNABP CCS811 DHT11 ATMEL STUDIO/TTNABP CCS811 DHT11 ATMEL STUDIO/TTNABP CCS811 DHT11 ATMEL STUDIO/ArduinoCore/library.cpp
@@ -0,0 +1,16 @@
+/*
+ * ArduinoCore.cpp
+ *
+ * Created: 26-6-2020 20:59:17
+ * Author : Sietze Koonstra
+ */ 
+
+#include <avr/io.h>
+
+
+/* Replace with your library code */
+int myfunc(void)
+{
+	return 0;
+}
+
---
+++ TTNABP CCS811 DHT11 ATMEL STUDIO/TTNABP CCS811 DHT11 ATMEL STUDIO/TTNABP CCS811 DHT11 ATMEL STUDIO/ttn-abp/Sketch.cpp
@@ -0,0 +1,295 @@
+﻿/*Begining of Auto generated code by Atmel studio */
+#include <Arduino.h>
+
+/*End of auto generated code by Atmel studio */
+
+/*******************************************************************************
+ * Copyright (c) 2015 Thomas Telkamp and Matthijs Kooijman
+ *
+ * Permission is hereby granted, free of charge, to anyone
+ * obtaining a copy of this document and accompanying files,
+ * to do whatever they want with them without any restriction,
+ * including, but not limited to, copying, modification and redistribution.
+ * NO WARRANTY OF ANY KIND IS PROVIDED.
+ *
+ * This example sends a valid LoRaWAN packet with payload "Hello,
+ * world!", using frequency and encryption settings matching those of
+ * the The Things Network.
+ *
+ * This uses ABP (Activation-by-personalisation), where a DevAddr and
+ * Session keys are preconfigured (unlike OTAA, where a DevEUI and
+ * application key is configured, while the DevAddr and session keys are
+ * assigned/generated in the over-the-air-activation procedure).
+ *
+ * Note: LoRaWAN per sub-band duty-cycle limitation is enforced (1% in
+ * g1, 0.1% in g2), but not the TTN fair usage policy (which is probably
+ * violated by this sketch when left running for longer)!
+ *
+ * To use this sketch, first register your application and device with
+ * the things network, to set or generate a DevAddr, NwkSKey and
+ * AppSKey. Each device should have their own unique values for these
+ * fields.
+ *
+ * Do not forget to define the radio type correctly in config.h.
+ * test
+ *
+ *******************************************************************************/
+
+#include <SPI.h>
+#include <Wire.h>
+#include <lmic.h>
+#include <hal/hal.h>
+#include <dht.h>
+#include "SparkFunCCS811.h"
+
+
+
+#define DHT11_PIN 3
+#define CCS811_ADDR 0x5B
+
+dht DHT;
+CCS811 mySensor(CCS811_ADDR);
+
+// LoRaWAN NwkSKey, network session key
+// This is the default Semtech key, which is used by the early prototype TTN
+// network.
+static const PROGMEM u1_t NWKSKEY[16] = { 0x20, 0x8C, 0xEF, 0x9A, 0x3C, 0xBB, 0xB4, 0x9B, 0x08, 0x45, 0x4C, 0x5E, 0xAF, 0x8C, 0x55, 0xEA };
+
+// LoRaWAN AppSKey, application session key
+// This is the default Semtech key, which is used by the early prototype TTN
+// network.
+static const u1_t PROGMEM APPSKEY[16] = { 0xBC, 0x69, 0xC6, 0x0B, 0xF7, 0x48, 0xA1, 0x4C, 0x29, 0x76, 0x1C, 0xD8, 0xF5, 0xE4, 0x3D, 0x41 };
+
+// LoRaWAN end-device address (DevAddr)
+static const u4_t DEVADDR = 0x2601143D; // <-- Change this address for every node!
+
+// These callbacks are only used in over-the-air activation, so they are
+// left empty here (we cannot leave them out completely unless
+// DISABLE_JOIN is set in config.h, otherwise the linker will complain).
+void os_getArtEui (u1_t* buf) { }
+void os_getDevEui (u1_t* buf) { }
+void os_getDevKey (u1_t* buf) { }
+
+static uint8_t bytesToSend[8];
+static osjob_t sendjob;
+
+// Schedule TX every this many seconds (might become longer due to duty
+// cycle limitations).
+const unsigned TX_INTERVAL = 30;
+
+// Pin mapping
+const lmic_pinmap lmic_pins = {
+    .nss = 10,
+    .rxtx = LMIC_UNUSED_PIN,
+    .rst = 9,
+    .dio = {2, 6, 7},
+};
+
+void updateSensorData() {
+	// read DHT11 sensor data
+	DHT.read11(DHT11_PIN);
+	float hum = DHT.humidity;
+	float temp = DHT.temperature;
+	uint8_t hum_t = (uint8_t) hum;						// whole part of the float
+	uint8_t temp_t = (uint8_t) temp;
+	String hum_frac = String(hum - hum_t, 1);			// extract the fractional part using substring
+	String temp_frac = String(temp - temp_t, 1);
+	uint8_t hum_f = hum_frac.substring(2).toInt();
+	uint8_t temp_f = temp_frac.substring(2).toInt();
+	uint8_t com_f = ((uint8_t) (hum_f << 4)) | ((uint8_t) temp_f);	// combine both fraction parts in one byte
+	Serial.println(String("whole: ") + String(hum_t) + String(temp_t));
+	Serial.println("fractional: " + String(hum_f) + ", " +  String(temp_f));
+	Serial.println("combinedFractional: " + String((uint8_t) bytesToSend[2]));
+	Serial.println("actual: " + String(hum, 1) + String(temp, 1));
+	//	Serial.println(String((uint8_t) bytesToSend[0]) + "," + String((uint8_t) bytesToSend[1]) + "," + String((uint8_t) bytesToSend[2]));
+	bytesToSend[0] = hum_t;
+	bytesToSend[1] = temp_t;
+	bytesToSend[2] = com_f;
+	//Serial.println(String((uint8_t) bytesToSend[2]));
+
+	//read CCS811 sensor data
+	if (mySensor.dataAvailable())
+	{
+		Serial.println("is available");
+		// use DHT11 data to compensate for the environment
+		mySensor.setEnvironmentalData(hum, temp);
+		mySensor.readAlgorithmResults();
+		uint16_t CO2 = mySensor.getCO2();
+		Serial.println(CO2);
+		uint8_t CO2_MSB = (uint8_t)(CO2 >> 8);
+		uint8_t CO2_LSB = (uint8_t)(CO2 & 0x00FF);
+
+		uint16_t tVOC = mySensor.getTVOC();
+		Serial.println(tVOC);
+		uint8_t tVOC_MSB = (uint8_t)(tVOC >> 8);
+		uint8_t tVOC_LSB = (uint8_t)(tVOC & 0x00FF);
+		bytesToSend[3] = CO2_MSB;
+		bytesToSend[4] = CO2_LSB;
+		bytesToSend[5] = tVOC_MSB;
+		bytesToSend[6] = tVOC_LSB;
+		} else {
+		Serial.println("data is not available!");
+	}
+}
+
+void do_send(osjob_t* j){
+	// Check if there is not a current TX/RX job running
+	if (LMIC.opmode & OP_TXRXPEND) {
+		Serial.println(F("OP_TXRXPEND, not sending"));
+		} else {
+		// Read sensor data and write to bytesToSend
+		updateSensorData();
+		// Prepare upstream data transmission at the next possible time.
+		LMIC_setTxData2(1, bytesToSend, sizeof(bytesToSend)-1, 0);
+		Serial.println(F("Packet queued"));
+	}
+	// Next TX is scheduled after TX_COMPLETE event.
+}
+
+void onEvent (ev_t ev) {
+    Serial.print(os_getTime());
+    Serial.print(": ");
+    switch(ev) {
+        case EV_SCAN_TIMEOUT:
+            Serial.println(F("EV_SCAN_TIMEOUT"));
+            break;
+        case EV_BEACON_FOUND:
+            Serial.println(F("EV_BEACON_FOUND"));
+            break;
+        case EV_BEACON_MISSED:
+            Serial.println(F("EV_BEACON_MISSED"));
+            break;
+        case EV_BEACON_TRACKED:
+            Serial.println(F("EV_BEACON_TRACKED"));
+            break;
+        case EV_JOINING:
+            Serial.println(F("EV_JOINING"));
+            break;
+        case EV_JOINED:
+            Serial.println(F("EV_JOINED"));
+            break;
+        case EV_RFU1:
+            Serial.println(F("EV_RFU1"));
+            break;
+        case EV_JOIN_FAILED:
+            Serial.println(F("EV_JOIN_FAILED"));
+            break;
+        case EV_REJOIN_FAILED:
+            Serial.println(F("EV_REJOIN_FAILED"));
+            break;
+        case EV_TXCOMPLETE:
+            Serial.println(F("EV_TXCOMPLETE (includes waiting for RX windows)"));
+            if (LMIC.txrxFlags & TXRX_ACK)
+              Serial.println(F("Received ack"));
+            if (LMIC.dataLen) {
+              Serial.println(F("Received "));
+              Serial.println(LMIC.dataLen);
+              Serial.println(F(" bytes of payload"));
+            }
+            // Schedule next transmission
+            os_setTimedCallback(&sendjob, os_getTime()+sec2osticks(TX_INTERVAL), do_send);
+            break;
+        case EV_LOST_TSYNC:
+            Serial.println(F("EV_LOST_TSYNC"));
+            break;
+        case EV_RESET:
+            Serial.println(F("EV_RESET"));
+            break;
+        case EV_RXCOMPLETE:
+            // data received in ping slot
+            Serial.println(F("EV_RXCOMPLETE"));
+            break;
+        case EV_LINK_DEAD:
+            Serial.println(F("EV_LINK_DEAD"));
+            break;
+        case EV_LINK_ALIVE:
+            Serial.println(F("EV_LINK_ALIVE"));
+            break;
+         default:
+            Serial.println(F("Unknown event"));
+            break;
+    }
+}
+
+void setup() {
+    Serial.begin(115200);
+    Serial.println(F("Starting"));
+
+    Wire.begin();
+
+    if (mySensor.begin() == false)
+	{
+		Serial.print("CCS811 error. Please check wiring. Freezing...");
+	    while (1);
+	}
+
+    // LMIC init
+    os_init();
+    // Reset the MAC state. Session and pending data transfers will be discarded.
+    LMIC_reset();
+
+    // Set static session parameters. Instead of dynamically establishing a session
+    // by joining the network, precomputed session parameters are be provided.
+    #ifdef PROGMEM
+    // On AVR, these values are stored in flash and only copied to RAM
+    // once. Copy them to a temporary buffer here, LMIC_setSession will
+    // copy them into a buffer of its own again.
+    uint8_t appskey[sizeof(APPSKEY)];
+    uint8_t nwkskey[sizeof(NWKSKEY)];
+    memcpy_P(appskey, APPSKEY, sizeof(APPSKEY));
+    memcpy_P(nwkskey, NWKSKEY, sizeof(NWKSKEY));
+    LMIC_setSession (0x1, DEVADDR, nwkskey, appskey);
+    #else
+    // If not running an AVR with PROGMEM, just use the arrays directly
+    LMIC_setSession (0x1, DEVADDR, NWKSKEY, APPSKEY);
+    #endif
+
+    #if defined(CFG_eu868)
+    // Set up the channels used by the Things Network, which corresponds
+    // to the defaults of most gateways. Without this, only three base
+    // channels from the LoRaWAN specification are used, which certainly
+    // works, so it is good for debugging, but can overload those
+    // frequencies, so be sure to configure the full frequency range of
+    // your network here (unless your network autoconfigures them).
+    // Setting up channels should happen after LMIC_setSession, as that
+    // configures the minimal channel set.
+    // NA-US channels 0-71 are configured automatically
+    LMIC_setupChannel(0, 868100000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
+    LMIC_disableChannel(1);
+	LMIC_disableChannel(2);
+//    LMIC_setupChannel(1, 868300000, DR_RANGE_MAP(DR_SF12, DR_SF7B), BAND_CENTI);      // g-band
+//    LMIC_setupChannel(2, 868500000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
+//    LMIC_setupChannel(3, 867100000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
+//    LMIC_setupChannel(4, 867300000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
+//    LMIC_setupChannel(5, 867500000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
+//    LMIC_setupChannel(6, 867700000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
+//    LMIC_setupChannel(7, 867900000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
+//    LMIC_setupChannel(8, 868800000, DR_RANGE_MAP(DR_FSK,  DR_FSK),  BAND_MILLI);      // g2-band
+    // TTN defines an additional channel at 869.525Mhz using SF9 for class B
+    // devices' ping slots. LMIC does not have an easy way to define set this
+    // frequency and support for class B is spotty and untested, so this
+    // frequency is not configured here.
+    #elif defined(CFG_us915)
+    // NA-US channels 0-71 are configured automatically
+    // but only one group of 8 should (a subband) should be active
+    // TTN recommends the second sub band, 1 in a zero based count.
+    // https://github.com/TheThingsNetwork/gateway-conf/blob/master/US-global_conf.json
+    LMIC_selectSubBand(1);
+    #endif
+
+    // Disable link check validation
+    LMIC_setLinkCheckMode(0);
+
+    // TTN uses SF9 for its RX2 window.
+    LMIC.dn2Dr = DR_SF9;
+
+    // Set data rate and transmit power for uplink (note: txpow seems to be ignored by the library)
+    LMIC_setDrTxpow(DR_SF7,14);
+
+    // Start job
+    do_send(&sendjob);
+}
+
+void loop() {
+    os_runloop_once();
+}
